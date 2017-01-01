9139234d21afe18caddadc814afa90dd0da43ac7
Asa Romberger <asa.romberger@gmail.com>
2017-01-01 11:44:32 -0800

    Jira-4 Add User modified:   ../Gemfile modified:   ../Gemfile.lock modified:   controllers/application_controller.rb new file:   controllers/sessions_controller.rb new file:   helpers/sessions_helper.rb new file:   models/user.rb new file:   views/layouts/_flashes.html.erb modified:   views/layouts/_menu.html.erb modified:   views/layouts/_menu_jira.html.erb new file:   views/layouts/_menu_user.html.erb modified:   views/layouts/application.html.erb new file:   views/sessions/new.html.erb modified:   ../config/routes.rb new file:   ../db/migrate/20161230013927_create_users.rb modified:   ../db/schema.rb new file:   ../test/fixtures/users.yml new file:   ../test/models/user_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227191259) do
+ActiveRecord::Schema.define(version: 20161230013927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20161227191259) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "signin"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
