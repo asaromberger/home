69e3fd92b308e06f5bb15b752a70c9808bb8cc32
Asa Romberger <asa.romberger@gmail.com>
2017-01-02 09:24:31 -0800

    Jira-6 Add Category new file:   models/category.rb new file:   ../db/migrate/20170102172258_create_categories.rb modified:   ../db/schema.rb new file:   ../test/fixtures/categories.yml new file:   ../test/models/category_test.rb

---
+++ db/migrate/20170102172258_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :categories do |t|
+      t.string :type
+      t.string :category
+      t.string :subcategory
+      t.boolean :tax
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102004358) do
+ActiveRecord::Schema.define(version: 20170102172258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "type"
+    t.string   "category"
+    t.string   "subcategory"
+    t.boolean  "tax"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "jiras", force: :cascade do |t|
     t.string   "summary"
