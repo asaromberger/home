1f7380c8c449761d97f4e957318137f9be22c1c9
Asa Romberger <asa.romberger@gmail.com>
2017-01-02 10:09:30 -0800

    Jira-6 Change tax to string renamed:    ../db/migrate/20170102172258_create_categories.rb -> ../db/migrate/20170102180627_create_categories.rb modified:   ../db/schema.rb modified:   ../test/fixtures/categories.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102172258) do
+ActiveRecord::Schema.define(version: 20170102180627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170102172258) do
     t.string   "type"
     t.string   "category"
     t.string   "subcategory"
-    t.boolean  "tax"
+    t.string   "tax"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
