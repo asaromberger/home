9e97b002eb4e53b1516f584d852a215e78a67829
Asa Romberger <asa.romberger@gmail.com>
2017-01-08 14:03:04 -0800

    Jira-20 modified:   controllers/bulkinput_controller.rb modified:   views/bulkinput/create.html.erb renamed:    views/bulkinput/inputfromtracking.html.erb -> views/bulkinput/edit.html.erb modified:   views/layouts/_menu_expenses.html.erb

--- app/controllers/bulkinput_controller.rb
+++ app/controllers/bulkinput_controller.rb
@@ -8,8 +8,8 @@ class BulkinputController < ApplicationController
 		@whatmaps = WhatMap.all.order('whatmap')
 	end
 
-	def inputfromtracking
-		@title = 'Bulk Input from Flat File'
+	def edit
+		@title = 'Bulk Input'
 	end
 
 	def create
@@ -22,69 +22,176 @@ class BulkinputController < ApplicationController
 		@errors = []
 		lines = @input.split("\n")
 		@table = Hash.new
-		@whatlist = [['', 0]]
-		What.all.order('what').each do |what|
-			@whatlist.push([what.what, what.id])
-		end
 		@categorylist = [['', 0]]
 		Category.all.order('ctype, category, subcategory, tax').each do |category|
 			@categorylist.push(["#{category.ctype}/#{category.category}/#{category.subcategory}/#{category.tax}", category.id])
 		end
 		lineno = 0
-		lines.each do |line|
-			fields = line.split("\t")
-			if fields[4]
-				lineno = lineno + 1
-				@table[lineno] = Hash.new
-				date = fields[0].gsub(/^\s*/, '').gsub(/\s*$/, '')
-				pm = fields[1].gsub(/^\s*/, '').gsub(/\s*$/, '')
-				check = fields[2].gsub(/^\s*/, '').gsub(/\s*$/, '')
-				what = fields[3].gsub(/^\s*/, '').gsub(/\s*$/, '')
-				amount = fields[4].gsub(/^\s*/, '').gsub(/\s*$/, '')
-				if date.match('^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]$')
-					# MM/DD/YYYY
-					month = date[0..1]
-					day = date[3..4]
-					year = date[6..9]
-				elsif date.match('^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$')
-					# YYYY-MM-DD
-					month = date[5..6]
-					day = date[8..9]
-					year = date[0..3]
-				else
-					@errors.push("BAD DATE: #{date}")
-					month = '01'
-					day = '01'
-					year = '1000'
-				end
-				date = "#{year}-#{month}-#{day}"
-				@table[lineno]['date'] = date
-				@table[lineno]['pm'] = pm
-				@table[lineno]['check'] = check
-				@table[lineno]['what'] = what
-				@table[lineno]['amount'] = amount
-				whatmap = WhatMap.where("whatmap = ?", what)
-				if whatmap.count > 0
-					whatmap = whatmap.first.what_id
-				else
-					whatmap = 0
-				end
-				@table[lineno]['whatmap'] = whatmap
-				item = Item.where("date = ? AND pm = ? AND checkno = ? AND amount = ?", date, pm, check, amount)
-				if item.count > 0
-					item = item.first
-					@table[lineno]['status'] = 'exists'
-					if item.what_id.blank?
-						@table[lineno]['category'] = 0
+		maxlineno = 30
+		if lines[0].gsub(/:.*/, '') == 'OFXHEADER'
+			# Quicken input
+			accountmap = Hash.new
+			accountmap['0014942631'] = 'savings'
+			accountmap['0014942658'] = 'checking'
+			accountmap['0026599074'] = 'fairview'
+			accountmap['0050148729'] = 'business'
+			accountmap['5420396180931795'] = 'mastercard'
+			accountmap['5420396180938550'] = 'mastercard'
+			accountmap['5420396180941950'] = 'mastercard'
+			accountmap['5420396180939475'] = 'mastercard'
+			accountmap['5420396180942115'] = 'mastercard'
+			accountmap['4046440000274510'] = 'mastercard'
+			# remerge lines
+			tmp = ''
+			lines.each do |line|
+				tmp = "#{tmp}#{line}"
+			end
+			lines = tmp.split("<");
+			type = '';
+			date = '';
+			amount = '';
+			pm = '';
+			fitid = '';
+			check = '';
+			what = '';
+			account = 'UNKNOWN';
+			lines.each do |line|
+				key = line.gsub(/>.*/, '')
+				line = line.gsub(/.*>/, '')
+				if key == 'DTPOSTED'
+					posted = line
+					year = line[0..3]
+					month = line[4..5]
+					day = line[6..7]
+					date = "#{year}-#{month}-#{day}"
+				elsif key == 'TRNAMT'
+					amount = line
+					if amount[0..0] == '-'
+						pm = '-'
+						amount = amount.gsub(/-/, '')
 					else
-						@table[lineno]['category'] = item.what.category_id
+						pm = '+'
+					end
+				elsif key == 'CHECKNUM'
+					check = line
+				elsif key == 'NAME'
+					what = line.gsub(/&amp;/, '&')
+				elsif key == '/STMTTRN'
+					what = "#{account}:#{what}"
+					whatlist = WhatMap.where("whatmap = ?", what)
+					if whatlist.count > 0
+						what_ids = whatlist.pluck('DISTINCT what_id')
+					else
+						tmp = What.where("what = ?", what)
+						if tmp.count > 0
+							what_ids = tmp.pluck('DISTINCT id')
+						else
+							what_ids = [0]
+						end
+					end
+					@whatlist = [['', 0]]
+					whatlist.each do |wl|
+						@whatlist.push([wl.whatmap, wl.id])
+					end
+					item = Item.where("date = ? AND pm = ? and checkno = ? and what_id IN (?) AND amount = ?", date, pm, check, what_ids, amount)
+					if item.count > 0
+						@errors.push("EXISTS: #{date} #{item.first.what.what} #{amount}")
+					else
+						lineno = lineno + 1
+						if lineno > maxlineno
+							@errors.push("Limited to #{maxlineno} lines at a time")
+							break
+						end
+						@table[lineno] = Hash.new
+						@table[lineno]['date'] = date
+						@table[lineno]['pm'] = pm
+						@table[lineno]['check'] = check
+						@table[lineno]['what'] = what
+						@table[lineno]['amount'] = amount
+						@table[lineno]['whatmaplist'] = @whatlist
+						@table[lineno]['status'] = 'new'
+						@table[lineno]['category'] = 0
+					end
+					type = '';
+					date = '';
+					amount = '';
+					pm = '';
+					fitid = '';
+					check = '';
+					what = '';
+				elsif key == 'ACCTID'
+					if accountmap[line].blank?
+						@errors.push("UNKNOWN ACCOUNT: #{line}")
+						account = 'UNKNOWN'
+					else
+						account = accountmap[line]
+					end
+				end
+			end
+		else
+			# Flat File Input
+			lines.each do |line|
+				fields = line.split("\t")
+				if fields[4]
+					date = fields[0].gsub(/^\s*/, '').gsub(/\s*$/, '')
+					pm = fields[1].gsub(/^\s*/, '').gsub(/\s*$/, '')
+					check = fields[2].gsub(/^\s*/, '').gsub(/\s*$/, '')
+					what = fields[3].gsub(/^\s*/, '').gsub(/\s*$/, '')
+					amount = fields[4].gsub(/^\s*/, '').gsub(/\s*$/, '')
+					if date.match('^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]$')
+						# MM/DD/YYYY
+						month = date[0..1]
+						day = date[3..4]
+						year = date[6..9]
+					elsif date.match('^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$')
+						# YYYY-MM-DD
+						month = date[5..6]
+						day = date[8..9]
+						year = date[0..3]
+					else
+						@errors.push("BAD DATE: #{date}")
+						month = '01'
+						day = '01'
+						year = '1000'
+					end
+					date = "#{year}-#{month}-#{day}"
+					whatlist = WhatMap.where("whatmap = ?", what)
+					if whatlist.count > 0
+						what_ids = whatlist.pluck('DISTINCT what_id')
+					else
+						tmp = What.where("what = ?", what)
+						if tmp.count > 0
+							what_ids = tmp.pluck('DISTINCT id')
+						else
+							what_id = [0]
+						end
+					end
+					@whatlist = [['', 0]]
+					whatlist.each do |wl|
+						@whatlist.push([wl.whatmap, wl.id])
+					end
+					item = Item.where("date = ? AND pm = ? and checkno = ? and what_id IN (?) AND amount = ?", date, pm, check, what_ids, amount)
+					if item.count > 0
+						@errors.push("EXISTS: #{date} #{item.first.what.what} #{amount}")
+					else
+						lineno = lineno + 1
+						if lineno > maxlineno
+							@errors.push("Limited to #{maxlineno} lines at a time")
+							break
+						end
+						@table[lineno] = Hash.new
+						@table[lineno]['date'] = date
+						@table[lineno]['pm'] = pm
+						@table[lineno]['check'] = check
+						@table[lineno]['what'] = what
+						@table[lineno]['amount'] = amount
+						@table[lineno]['whatmaplist'] = @whatlist
+						@table[lineno]['status'] = 'new'
+						@table[lineno]['category'] = 0
 					end
 				else
-					@table[lineno]['status'] = 'new'
-					@table[lineno]['category'] = 0
+					@errors.push("BAD LINE: #{line}")
 				end
-			else
-				@errors.push("BAD LINE: #{line}")
 			end
 		end
 	end
@@ -94,8 +201,8 @@ class BulkinputController < ApplicationController
 		@title = 'Classify Bulk Input'
 		@input = params[:input]
 		@errors = []
-
 		@table = Hash.new
+		lineno = 0
 		params[:table].each do |id, values|
 			status = values['status']
 			date = values['date']
@@ -103,94 +210,84 @@ class BulkinputController < ApplicationController
 			check = values['check']
 			what = values['what']
 			amount = values['amount']
+			mapto = values['mapto']
 			whatmap = values['whatmap'].to_i
 			category = values['category'].to_i
-			item = Item.where("date = ? AND pm = ? AND checkno = ? AND amount = ?", date, pm, check, amount)
-			if status == 'exists' && whatmap > 0
-				# item exists and has a what specified
-				# update the item to point to the what
-				item = item.first
-				item.what_id = whatmap;
-				item.save
-				# update whatmap
-				twhatmap = WhatMap.where("whatmap = ?", what)
-				if twhatmap.count > 0
-					twhatmap = twhatmap.first
-				else
-					twhatmap = WhatMap.new
-					twhatmap.whatmap = what
-				end
-				twhatmap.what_id = whatmap
-				twhatmap.save
+			if mapto.blank?
+				newwhat = what
 			else
-				@table[id] = Hash.new
-				if whatmap > 0
-					# whatmap specified
-					# create or update item
-					if item.count > 0
-						item = item.first
-					else
-						item = Item.new
-						item.date = date
-						item.pm = pm
-						item.checkno = check
-						item.amount = amount
-					end
-					item.what_id = whatmap
-					item.save
-					@table[id]['status'] = 'exists'
-				elsif category > 0
-					# category specified
-					# create or update What
-					twhat = What.where("what = ?", what)
-					if twhat.count > 0
-						twhat = twhat.first
-					else
-						twhat = What.new
-						twhat.what = what
-					end
+				newwhat = mapto
+			end
+			if category > 0
+				twhat = What.where("what = ?", newwhat)
+				if twhat.count == 0
+					twhat = What.new
+					twhat.what = newwhat
 					twhat.category_id = category
 					twhat.save
-					twhat = What.where("what = ?", what).first
-					# create or update whatmap
-					twhatmap = WhatMap.where("whatmap = ?", what)
-					if twhatmap.count > 0
-						twhatmap = twhatmap.first
-					else
+				end
+				whatmap = What.where("what = ?", newwhat).first.id
+			end
+			if ! mapto.blank?
+				twhatmap = WhatMap.joins(:what).where("whatmap = ? AND whats.what = ?", what, mapto)
+				if twhatmap.count == 0
+					# need to create a new map
+					twhat = What.where("what = ?", mapto)
+					if twhat.count > 0
+						whatmap = twhat.first.id
 						twhatmap = WhatMap.new
 						twhatmap.whatmap = what
+						twhatmap.what_id = whatmap
+						twhatmap.save
 					end
-					twhatmap.what_id = twhat.id
-					twhatmap.save
-					whatmap = WhatMap.where("whatmap = ?", what).first.what_id
-					# create or update item
-					if item.count > 0
-						item = item.first
-					else
-						item = Item.new
-						item.date = date
-						item.pm = pm
-						item.checkno = check
-						item.amount = amount
-					end
-					item.what_id = whatmap
-					item.save
-					@table[id]['status'] = 'exists'
-				else
-					@table[id]['status'] = status
 				end
-				@table[id]['date'] = date
-				@table[id]['pm'] = pm
-				@table[id]['check'] = check
-				@table[id]['what'] = what
-				@table[id]['amount'] = amount
-				@table[id]['whatmap'] = whatmap
-				@table[id]['category'] = category
+				newwhat = mapto
+			else
+				newwhat = what
 			end
-		end
-		@whatlist = [['', 0]]
-		What.all.order('what').each do |what|
-			@whatlist.push([what.what, what.id])
+			item = Item.joins(:what).where("date = ? AND pm = ? AND checkno = ? AND what = ? AND amount = ?", date, pm, check, newwhat, amount)
+			if whatmap > 0
+				if item.count > 0
+					item = item.first
+				else
+					item = Item.new
+					item.date = date
+					item.pm = pm
+					item.checkno = check
+					item.amount = amount
+				end
+				item.what_id = whatmap
+				item.save
+			end
+			# recycle the item if it is not categorized
+			whatlist = [newwhat]
+			WhatMap.where("whatmap = ?", what).each do |map|
+				whatlist.push(map.what.what)
+			end
+			item = Item.joins(:what).where("date = ? AND pm = ? AND checkno = ? AND what IN (?) AND amount = ?", date, pm, check, whatlist, amount)
+			if item.count == 0
+				lineno = lineno + 1
+				@table[lineno] = Hash.new
+				@table[lineno]['date'] = date
+				@table[lineno]['pm'] = pm
+				@table[lineno]['check'] = check
+				@table[lineno]['what'] = what
+				@table[lineno]['amount'] = amount
+				@table[lineno]['whatmaplist'] = [['', 0]]
+				WhatMap.where("whatmap = ?", what).each do |wl|
+					@table[lineno]['whatmaplist'].push([wl.what.what, wl.what_id])
+				end
+				@table[lineno]['status'] = 'new'
+				twhat = What.where("what = ?", what)
+				if twhat.count > 0
+					@errors.push("#{date} #{what} #{twhat.first.category_id}")
+					@table[lineno]['category'] = twhat.first.category_id
+				else
+					@table[lineno]['category'] = 0
+				end
+			else
+				@errors.push("Item Complete: #{date} #{what} #{amount}")
+			end
 		end
 		@categorylist = [['', 0]]
 		Category.all.order('ctype, category, subcategory, tax').each do |category|
@@ -201,10 +298,6 @@ class BulkinputController < ApplicationController
 		else
 			redirect_to inputfromtracking_path, notice: "Entry Complete"
 		end
-	end
-
-	def edit
-	fail
 	end
 
 	def update
