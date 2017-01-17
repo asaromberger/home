963903f3a9ffa1ed95d874fc073e599fb2102534
Asa Romberger <asa.romberger@gmail.com>
2017-01-16 16:17:29 -0800

    Jira-89 Pass and display document name on completion modified:   controllers/bulkinput_controller.rb modified:   views/bulkinput/new.html.erb

--- app/controllers/bulkinput_controller.rb
+++ app/controllers/bulkinput_controller.rb
@@ -18,6 +18,7 @@ class BulkinputController < ApplicationController
 		Input.delete_all
 		@title = 'Classify Bulk Input'
 		@document = params[:document]
+		@documentname = @document.original_filename
 		File.open(Rails.root.join(@document.original_filename), 'wb') do |file|
 			@input = @document.read
 		end
@@ -135,7 +136,7 @@ class BulkinputController < ApplicationController
 				end
 			end
 		end
-		redirect_to new_bulkinput_path
+		redirect_to new_bulkinput_path(documentname: @documentname)
 	end
 
 	def new
@@ -254,7 +255,7 @@ class BulkinputController < ApplicationController
 			@categorylist.push(["#{category.ctype}/#{category.category}/#{category.subcategory}/#{category.tax}", category.id])
 		end
 		if @table.count == 0
-			redirect_to edit_bulkinput_path(id: 0), notice: "Entry Complete"
+			redirect_to edit_bulkinput_path(id: 0), notice: "File #{params[:documentname]} is complete"
 		end
 	end
 
