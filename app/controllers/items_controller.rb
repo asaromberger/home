5e4571b4fb19a6ab94762b12eb74cfcdc1593a35
Asa Romberger <asa.romberger@gmail.com>
2017-01-20 14:44:22 -0800

    Jira-91 Year Expenses year select in reverse order transfer year through views default add to - modified:   controllers/items_controller.rb modified:   views/items/_form.html.erb modified:   views/items/index.html.erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,10 @@ class ItemsController < ApplicationController
 	before_action :require_expenses
 
 	def index
-		if params[:year]
-			@year = params[:year]
-		else
+		if params[:year].blank?
 			@year = Time.now.year
+		else
+			@year = params[:year]
 		end
 		@title = "#{@year} Expenses"
 		@items = Item.where("EXTRACT(year FROM date) = ?", @year).order('date')
@@ -15,44 +15,50 @@ class ItemsController < ApplicationController
 		Item.all.pluck("DISTINCT EXTRACT(year FROM date)").each do |year|
 			@years.push(year.to_i)
 		end
-		@years = @years.sort
+		@years = @years.sort.reverse
 	end
 
 	def new
 		@title = 'New Item'
+		@year = params[:year]
 		@item = Item.new
-		@item.date = Date.new(params[:year].to_i, 1, 1)
+		@item.date = Date.new(@year.to_i, 1, 1)
+		@item.pm = '-'
 		@whats = What.all.order('what')
 	end
 
 	def create
 		@item = Item.new(item_params)
+		@year = params[:year]
 		if @item.save
-			redirect_to items_path, notice: 'Item Added'
+			redirect_to items_path(year: @year), notice: 'Item Added'
 		else
-			redirect_to items_path, alert: 'Failed to add Item'
+			redirect_to items_path(year: @year), alert: 'Failed to add Item'
 		end
 	end
 
 	def edit
 		@title = 'Edit Item'
+		@year = params[:year]
 		@item = Item.find(params[:id])
 		@whats = What.all.order('what')
 	end
 
 	def update
+		@year = params[:year]
 		@item = Item.find(params[:id])
 		if @item.update(item_params)
-			redirect_to items_path, notice: 'Item Updated'
+			redirect_to items_path(year: @year), notice: 'Item Updated'
 		else
-			redirect_to items_path, alert: 'Failed to update Item'
+			redirect_to items_path(year: @year), alert: 'Failed to update Item'
 		end
 	end
 
 	def destroy
+		@year = params[:year]
 		@item = Item.find(params[:id])
 		@item.delete
-		redirect_to items_path, notice: 'Item #{@item.date} #{@item.what.what} Deleted'
+		redirect_to items_path(year: @year), notice: "Item #{@item.date} #{@item.what.what} Deleted"
 	end
 
 private
