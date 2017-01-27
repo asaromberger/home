1955ecdccd6411a1bd930d3c5b2db0905ab08de5
Asa Romberger <asa.romberger@gmail.com>
2017-01-26 17:39:34 -0800

    Jira-21 Expenses / Unused new file:   controllers/unused_controller.rb modified:   views/layouts/_menu_expenses.html.erb new file:   views/unused/index.html.erb modified:   ../config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,8 @@ Rails.application.routes.draw do
 
 	resources :transfers
 
+	resources :unused
+
 	match '/admin_roles', to: 'admin#roles', via: :get
 	match '/admin_roles_edit', to: 'admin#roles_edit', via: :get
 	match '/admin_roles_update', to: 'admin#roles_update', via: :get
