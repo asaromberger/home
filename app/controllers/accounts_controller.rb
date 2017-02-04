d1f63b83c3a69de8e9abc487800b0fb2e3340896
Asa Romberger <asa.romberger@gmail.com>
2017-02-04 13:24:09 -0800

    Jira-107 Investments / Account Management new file:   controllers/accounts_controller.rb new file:   models/account.rb new file:   views/accounts/_form.html.erb new file:   views/accounts/edit.html.erb new file:   views/accounts/index.html.erb new file:   views/accounts/new.html.erb modified:   views/layouts/_menu_investments.html.erb modified:   ../config/routes.rb new file:   ../db/migrate/20170204205550_create_accounts.rb modified:   ../db/schema.rb new file:   ../test/fixtures/accounts.yml new file:   ../test/models/account_test.rb

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,59 @@
+class AccountsController < ApplicationController
+
+	before_action :require_signed_in
+	before_action :require_investments
+
+	def index
+		@title = 'Investment Accounts'
+		@accounts = Account.all.order('account')
+	end
+
+	def new
+		@title = 'New Account'
+		@account = Account.new
+		@atypes = [['cash'], ['brokerage'], ['annuity']]
+	end
+
+	def create
+		@account = Account.new(account_params)
+		if @account.save
+			redirect_to accounts_path, notice: 'Account Added'
+		else
+			redirect_to accounts_path, alert: 'Failed to create Account'
+		end
+	end
+
+	def edit
+		@title = 'Edit Account'
+		@account = Account.find(params[:id])
+		@atypes = [['cash'], ['brokerage'], ['annuity']]
+	end
+
+	def update
+		@account = Account.find(params[:id])
+		if @account.update(account_params)
+			redirect_to accounts_path, notice: 'Account Updated'
+		else
+			redirect_to accounts_path, alert: 'Failed to update Account'
+		end
+	end
+
+	def destroy
+		@account = Account.find(params[:id])
+		@account.delete
+		redirect_to accounts_path, notice: "Account #{@account.account} Deleted"
+	end
+
+private
+	
+	def account_params
+		params.require(:account).permit(:account, :atype)
+	end
+
+	def require_investments
+		unless has_role(current_user.id, 'investments')
+			redirect_to root_url, alert: "inadequate permissions"
+		end
+	end
+
+end
