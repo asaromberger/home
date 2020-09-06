e8afacbf64b978537c0b0beff789df1a9d18da80
Asa Romberger <asa.romberger@gmail.com>
2020-09-05 17:02:22 -0700

    HOME-125-RemoveConversations Delete Conversations / People modified:   controllers/admin_controller.rb deleted:    controllers/conversations_controller.rb deleted:    controllers/people_controller.rb deleted:    models/conversation.rb deleted:    models/person.rb modified:   models/user.rb deleted:    views/conversations/_form.html.erb deleted:    views/conversations/edit.html.erb deleted:    views/conversations/index.html.erb deleted:    views/conversations/new.html.erb modified:   views/layouts/_menu.html.erb deleted:    views/people/_form.html.erb deleted:    views/people/edit.html.erb deleted:    views/people/index.html.erb deleted:    views/people/new.html.erb new file:   ../db/migrate/20200905234351_drop_conversations.rb new file:   ../db/migrate/20200905235542_drop_people.rb modified:   ../db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -2,156 +2,140 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224210029) do
+ActiveRecord::Schema.define(version: 2020_09_05_235542) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "hstore"
+  enable_extension "plpgsql"
 
-  create_table "accountmaps", force: :cascade do |t|
-    t.string   "account"
-    t.string   "ctype"
+  create_table "accountmaps", id: :serial, force: :cascade do |t|
+    t.string "account"
+    t.string "ctype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "accounts", force: :cascade do |t|
-    t.string   "account"
-    t.string   "atype"
+  create_table "accounts", id: :serial, force: :cascade do |t|
+    t.string "account"
+    t.string "atype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean  "closed"
+    t.boolean "closed"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "ctype"
-    t.string   "category"
-    t.string   "subcategory"
-    t.string   "tax"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer  "person_id"
-    t.date     "date"
-    t.text     "note"
-    t.date     "scheduled"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "ctype"
+    t.string "category"
+    t.string "subcategory"
+    t.string "tax"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "inputs", force: :cascade do |t|
-    t.date     "date"
-    t.string   "pm"
-    t.string   "checkno"
-    t.string   "what"
-    t.decimal  "amount"
+  create_table "inputs", id: :serial, force: :cascade do |t|
+    t.date "date"
+    t.string "pm"
+    t.string "checkno"
+    t.string "what"
+    t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "investment_maps", force: :cascade do |t|
-    t.integer  "account_id"
-    t.integer  "summary_type_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "investments", force: :cascade do |t|
-    t.integer  "account_id"
-    t.date     "date"
-    t.decimal  "value"
-    t.decimal  "shares"
-    t.decimal  "pershare"
-    t.decimal  "guaranteed"
+  create_table "investment_maps", id: :serial, force: :cascade do |t|
+    t.integer "account_id"
+    t.integer "summary_type_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.date     "date"
-    t.string   "pm"
-    t.string   "checkno"
-    t.integer  "what_id"
-    t.decimal  "amount"
+  create_table "investments", id: :serial, force: :cascade do |t|
+    t.integer "account_id"
+    t.date "date"
+    t.decimal "value"
+    t.decimal "shares"
+    t.decimal "pershare"
+    t.decimal "guaranteed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "jiras", force: :cascade do |t|
-    t.string   "summary"
-    t.string   "status"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "priority"
-  end
-
-  create_table "people", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
+  create_table "items", id: :serial, force: :cascade do |t|
+    t.date "date"
+    t.string "pm"
+    t.string "checkno"
+    t.integer "what_id"
+    t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "permissions", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "pkey"
-    t.hstore   "pvalue"
+  create_table "jiras", id: :serial, force: :cascade do |t|
+    t.string "summary"
+    t.string "status"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "priority"
+  end
+
+  create_table "permissions", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "pkey"
+    t.hstore "pvalue"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "rebalance_maps", force: :cascade do |t|
-    t.integer  "rebalance_type_id"
-    t.integer  "account_id"
-    t.decimal  "target"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-  end
-
-  create_table "rebalance_types", force: :cascade do |t|
-    t.string   "rtype"
+  create_table "rebalance_maps", id: :serial, force: :cascade do |t|
+    t.integer "rebalance_type_id"
+    t.integer "account_id"
+    t.decimal "target"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "summary_types", force: :cascade do |t|
-    t.string   "stype"
-    t.integer  "priority"
+  create_table "rebalance_types", id: :serial, force: :cascade do |t|
+    t.string "rtype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "signin"
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "what_maps", force: :cascade do |t|
-    t.string   "whatmap"
+  create_table "summary_types", id: :serial, force: :cascade do |t|
+    t.string "stype"
+    t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "what_id"
   end
 
-  create_table "whats", force: :cascade do |t|
-    t.string   "what"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "signin"
+    t.string "password_digest"
+    t.string "remember_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "what_maps", id: :serial, force: :cascade do |t|
+    t.string "whatmap"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "what_id"
+  end
+
+  create_table "whats", id: :serial, force: :cascade do |t|
+    t.string "what"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
